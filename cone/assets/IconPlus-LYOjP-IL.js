62025ad476c8add8b023373bd0af261d0e09c6fb
DSeller0 <ze_do_arthur@hotmail.com>
2026-09-04 21:52:14 +0000

    Deploying to gh-pages from @ DSeller0/CrossFit-Apps@bad96473156388dbaf758e98cb16561a8387f960 🚀

---
+++ cone/assets/IconPlus-LYOjP-IL.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createReactComponent-3sQ6LGZY.js";var t=e(`outline`,`pencil`,`Pencil`,[[`path`,{d:`M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4`,key:`svg-0`}],[`path`,{d:`M13.5 6.5l4 4`,key:`svg-1`}]]),n=e(`outline`,`plus`,`Plus`,[[`path`,{d:`M12 5l0 14`,key:`svg-0`}],[`path`,{d:`M5 12l14 0`,key:`svg-1`}]]);export{t as n,n as t};
